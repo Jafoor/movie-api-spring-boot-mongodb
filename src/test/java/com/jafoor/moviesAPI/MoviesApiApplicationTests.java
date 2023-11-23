4c8fcd041f010e45e93396c6022a7955de47ba39
abu_jafor <abu.jafor@tigerit.com>
2023-11-23 09:30:07 +0600

    init

---
+++ src/test/java/com/jafoor/moviesAPI/MoviesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jafoor.moviesAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
